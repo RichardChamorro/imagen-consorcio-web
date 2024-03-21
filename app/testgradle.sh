8eb0def0203dd2c747451349ff0abf9be9759391
RichardChamorro <154255822+RichardChamorro@users.noreply.github.com>
2024-03-21 13:52:11 -0300

    Update testgradle.sh

--- app/testgradle.sh
+++ app/testgradle.sh
@@ -5,5 +5,5 @@ NAV=$2
 # Ejecucion de prueba
 #gradle runWithCucumber -Ptags=$TAG -Pnav=$NAV | tee logGradle.txt || true
 gradle build --refresh-dependencies
-xvfb-run -a --server-args="-screen 0 1920x1080x24" gradle runWithCucumber -Ptags=$TAG -Pnav=$NAV || true
+#xvfb-run -a --server-args="-screen 0 1920x1080x24" gradle runWithCucumber -Ptags=$TAG -Pnav=$NAV || true
 ls /opt/framework/results
